b86d315751b798c91eb742c41041cd44c36cb8d6
Tiago Gordiano de Souza <tiagogordiano@gmail.com>
2023-06-20 09:14:47 -0300

    add recompensa de ação nao permitida para evitar loops

--- constantes.py
+++ constantes.py
@@ -11,8 +11,6 @@ estados_pocos = [2, 10, 15]
 estados_finais = [2, 8, 10, 15]
 estados_iniciais = [0, 1, 3, 4, 5, 6, 7, 11, 12, 13, 14]
 
-wumpus_vivo = True
-
 global estado_guerreiro
 
 CIMA = 0
@@ -25,4 +23,5 @@ ACOES = [CIMA, BAIXO, DIREITA, ESQUERDA, PEGAR]
 recompensa_estado_vazio = -1
 recompensa_poco = -1000
 recompensa_wumpus = -1000
+recompensa_acao_nao_permitida = -1000
 recompensa_pegar_ouro = 1000
--- teste.py
+++ teste.py
@@ -1,48 +1,36 @@
 # teste codigo de acoes
 
-from constantes import (estados_iniciais, estados_finais, ACOES)
+from constantes import (estados_iniciais, estados_finais, recompensa_acao_nao_permitida, ACOES)
 from funcoes import (acao_permitida, PEGAR, estado_ouro, novo_estado_guerreiro, recompensas)
 import random
 import numpy as np
 
-qt_episodios = 100
+qt_episodios = 200
 
 melhor_recompensa = 0
-melhor_episodio = 0
-qt_acoes_melhor_episodio = 0
-estado_final_melhor_episodio = 0
 vitorias = 0
 
 q_table = np.zeros((16, 5))
 
 # Hiperparametros
-alpha = 0.83 #taxa de aprendizado
-gamma = 0.95 #fator de desconto
-epsilon = 0.8 #parametro que auxilia a decidir entre exploration e explotation 
+alpha = 0.8 #taxa de aprendizado
+gamma = 0.8 #fator de desconto
+epsilon = 0.9 #parametro que auxilia a decidir entre exploration e explotation 
               #e evitar overfitting (escolha da mesma rota sempre)
 epsilon_min = 0.01
 
-tolerancia = 0.000001
-
-acoes_exploration = 0
-acoes_exploitation = 0
-
+tolerancia = 0.00001
 
 #           inicio do treinamento
-
+acoes_exploration = 0
+acoes_exploitation = 0
 episodios_executados = 0
 
 for episodio in range(qt_episodios):
     recompensa_episodio = 0
-    acoes_tomadas_episodio = 0
-    estado_final_episodio = 0
     estado_guerreiro = random.choice(estados_iniciais)
-    estados_finais_episodios = estados_finais
-    pegou_ouro = False
-    todas_acoes = []
-    todas_recompensas = []
-    todos_estados = []
-    todos_estados.append(estado_guerreiro)
+    estados_finais_episodios = []
+    estados_finais_episodios = estados_finais.copy()
     convergiu = False
 
     while estado_guerreiro not in estados_finais_episodios:
@@ -57,18 +45,13 @@ for episodio in range(qt_episodios):
             acoes_exploitation += 1
 
         if acao_permitida(estado_guerreiro, acao):
-            acoes_tomadas_episodio += 1
-            todas_acoes.append(acao)
 
             if(acao == PEGAR and estado_guerreiro == estado_ouro):
                 estados_finais_episodios.append(estado_ouro)
 
             proximo_estado_guerreiro = novo_estado_guerreiro(estado_guerreiro, acao)
 
-            todos_estados.append(proximo_estado_guerreiro)
-
             recompensa_acao = recompensas(proximo_estado_guerreiro, acao)
-            todas_recompensas.append(recompensa_acao)
             recompensa_episodio += recompensa_acao
 
             q_value_antigo = q_table[estado_guerreiro, acao]
@@ -80,7 +63,6 @@ for episodio in range(qt_episodios):
             q_table[estado_guerreiro, acao] = novo_q_value
             
             estado_guerreiro = proximo_estado_guerreiro
-            estado_final_episodio = estado_guerreiro
 
             dif = abs(novo_q_value - q_value_antigo)
 
@@ -88,49 +70,40 @@ for episodio in range(qt_episodios):
                 convergiu = True
                 break
 
+        #se a acao nao for permitida, colocamos uma penalidade muito alta
+        #assim, ao executar baseado na q_table, evita o guerreiro de ficar em loop
+        else:
+            recompensa_acao = recompensa_acao_nao_permitida
+            q_value_antigo = q_table[estado_guerreiro, acao]
+            maior_q_value_prox_estado = np.max(q_table[estado_guerreiro])
 
-    episodios_executados = episodios_executados + 1
+            #média móvel exponencial para atualização dos q-values na q_table
+            sample = alpha * (recompensa_acao + gamma * maior_q_value_prox_estado)
+            novo_q_value = (1 - alpha) * q_value_antigo + sample
+            q_table[estado_guerreiro, acao] = novo_q_value
+
+    episodios_executados += 1
     
     if estado_ouro in estados_finais_episodios:
-        vitorias = vitorias + 1
-
-    if(recompensa_episodio > melhor_recompensa or melhor_episodio == 0):
-        melhor_recompensa = recompensa_episodio
-        estado_final_melhor_episodio = estado_final_episodio
-        melhor_episodio = episodio
-        qt_acoes_melhor_episodio = acoes_tomadas_episodio
-        todas_acoes_melhor_episodio = todas_acoes
-        todas_recompensas_melhor_episodio = todas_recompensas
-        todos_estados_melhor_episodio = todos_estados
-
-    if convergiu:
-        break
+        vitorias += 1
 
     if epsilon > epsilon_min:
         epsilon = epsilon - epsilon_min
+    
+    if convergiu:
+        break
 
 #           fim do treinamento
-
-print("------------------------------------------------")
-print("Episodios executados: ", episodios_executados)
-print("Episodio escolhido: ", melhor_episodio)
-print("Recompensa: ", melhor_recompensa)
-print("Acoes: ", qt_acoes_melhor_episodio)
-print("Estado final: ", estado_final_melhor_episodio)
-
-print(todos_estados_melhor_episodio)
-print(todas_acoes_melhor_episodio)
-print(todas_recompensas_melhor_episodio)
-
-print("Estados finais: ", estados_finais)
+print("--------------Treinamento--------------------------------")
+print(f"{episodios_executados} episodios executados de {qt_episodios}")
 print("Vitorias: ", vitorias)
 print("% vitorias: ", vitorias*100/qt_episodios)
-
+print("Taxa de aprendizado (alpha): ", alpha)
+print("Fator de desconto (gamma): ", gamma)
 print("Epsilon final: " , epsilon)
 print("Acoes exploration: ", acoes_exploration)
 print("Acoes exploitation: ", acoes_exploitation)
-print("------------------------------------------------")
-
+print("--------------Fim do Treinamento--------------------------")
 
 #               avaliar os valores de q_table
 acoes_tomadas_todos_episodios = 0
@@ -139,14 +112,12 @@ vitorias = 0
 
 for episodio in range(qt_episodios):
 
-    estado_final_episodio = 0
     estado_guerreiro = random.choice(estados_iniciais)
     recompensa_episodio = 0
-    estados_finais_episodios = estados_finais
-    pegou_ouro = False
+    estados_finais_episodios.clear()
+    estados_finais_episodios = estados_finais.copy()
 
     while estado_guerreiro not in estados_finais_episodios:
-
         acao = np.argmax(q_table[estado_guerreiro])
 
         if acao_permitida(estado_guerreiro, acao):
@@ -168,17 +139,16 @@ for episodio in range(qt_episodios):
             q_table[estado_guerreiro, acao] = novo_q_value
             
             estado_guerreiro = proximo_estado_guerreiro
-            estado_final_episodio = estado_guerreiro
 
     recompensa_total_episodios += recompensa_episodio
-    
+
     if estado_ouro in estados_finais_episodios:
         vitorias = vitorias + 1
 
-print("------------------------------------------------")
+print("--------------Avaliando valores tabela q--------------------------------")
 
-print(f"Results after {qt_episodios} episodes:")
-print(f"Average timesteps per episode: {acoes_tomadas_todos_episodios / qt_episodios}")
-print(f"Average reward per episode: {recompensa_total_episodios / qt_episodios}")
+print(f"Resultados apos {qt_episodios} episodes:")
+print(f"Quantidade media de acoes tomadas por episodio: {acoes_tomadas_todos_episodios / qt_episodios}")
+print(f"Recompensa media por episodio: {recompensa_total_episodios / qt_episodios}")
 print("Vitorias: ", vitorias)
 print("% vitorias: ", (vitorias/qt_episodios)*100)
